c6dc4827adbbe2d099c7ad31dfbb09143da3dfba
test <test@bears-air.lan>
2024-05-25 19:47:50 -0400

    FIX: corrected first install script

--- install-first.sh
+++ install-first.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 xcode-select --install
-yes | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-python3 -m pip install ansible --user
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/test/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
